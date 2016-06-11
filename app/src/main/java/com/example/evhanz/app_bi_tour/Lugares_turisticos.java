77f7291c412a91d62907df12d4271f44ee84a1ce
Esteladiz <aletse.diz@gmail.com>
2016-06-11 09:30:34 -0500

    First Step

---
+++ app/src/main/java/com/example/evhanz/app_bi_tour/Lugares_turisticos.java
@@ -0,0 +1,13 @@
+package com.example.evhanz.app_bi_tour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lugares_turisticos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugares_turisticos);
+    }
+}
